9ccb93bd8a19763d2170866b85249e93ead5e83f
rojinaAmatya <rojinaamatya1432@gmail.com>
2020-02-21 10:18:14 -0600

    Created Problem 1 from Project Euler
    Sum of all the multipled of 3 or 5 below 1000

---
+++ problem1.js
@@ -0,0 +1,16 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5,
+//we get 3,5,6,and 9. The sum of these multiple is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000
+
+let sum = 0;
+let num = 10;
+
+for(let i = 0; i < num; i++){
+    if(i % 3 == 0 ||i % 5 == 0 )
+    {
+        sum = sum + i;
+    }
+}
+
+console.log(sum); //answer = 233168
